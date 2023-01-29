27cad9e56a60c0ecb1a4ca421ef673da89fade10
Vishal_Rohilla <rohillavishal510@gmail.com>
2023-01-29 17:22:04 +0530

    added fixed route

--- routes/product.route.js
+++ routes/product.route.js
@@ -32,16 +32,10 @@ ProductRouter.post('/create',async(req,res)=>{
 ProductRouter.patch('/update/:id',async(req,res)=>{
     const payload = req.body
     const ID = req.params.id
-    const product = await ProductModel.find({_id:ID})
-    const userID_in_product = product[0].adminID 
-    const userID_making_request = req.body.adminID
     try{
-        if(userID_in_product!=userID_making_request){
-            res.send('You are not authorized to do the operation')
-        }else{
+       
             await ProductModel.findByIdAndUpdate({_id:ID},payload)
         res.send(`Updated the product with id - ${ID}`)
-        }
         
     }catch(err){
         console.log(err)
@@ -52,16 +46,11 @@ ProductRouter.patch('/update/:id',async(req,res)=>{
 
 ProductRouter.delete('/delete/:id',async(req,res)=>{
     const ID = req.params.id
-    const note = await ProductModel.find({_id:ID})
-    const userID_in_product = note[0].adminID 
-    const userID_making_request = req.body.adminID
+    
     try{
-        if(userID_in_product!=userID_making_request){
-            res.send('You are not authorized')
-        }else{
+        
             await ProductModel.findByIdAndDelete({_id:ID})
         res.send(`Deleted the note with id - ${ID}`)
-        }
         
     }catch(err){
         console.log(err)
